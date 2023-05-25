528e77ec44251ae438b6050a6489c1e92b656ce4
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 02:41:10 -0700

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,31 +14,31 @@ resource "aws_s3_bucket" "example" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "example" {
+#   bucket = aws_s3_bucket.example.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+# resource "aws_s3_bucket_public_access_block" "example" {
+#   bucket = aws_s3_bucket.example.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.example,
-    aws_s3_bucket_public_access_block.example,
-  ]
+# resource "aws_s3_bucket_acl" "example" {
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.example,
+#     aws_s3_bucket_public_access_block.example,
+#   ]
 
-  bucket = aws_s3_bucket.example.id
-  acl    = "public-read"
-}
+#   bucket = aws_s3_bucket.example.id
+#   acl    = "public-read"
+# }
 
 resource "aws_s3_bucket_website_configuration" "example" {
   bucket = aws_s3_bucket.example.id
