3ed7cde6a23e00cf6c153a71291bda916f1b3861
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 02:46:17 -0700

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,8 @@ resource "aws_s3_bucket_website_configuration" "example" {
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
