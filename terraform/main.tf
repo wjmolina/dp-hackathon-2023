00f39d3dc8ac8978e75258063525c67be53b427e
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 02:19:56 -0700

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,37 +9,33 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "dp-hackathon-2023"
+resource "aws_s3_bucket" "example" {
+  bucket        = "dp-hackathon-2023"
+  force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_website_configuration" "static_website_bucket_website_configuration" {
-  bucket = aws_s3_bucket.static_website_bucket.id
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
 
-  index_document = "index.html"
-  error_document = "error.html"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
-  bucket = aws_s3_bucket.static_website_bucket.id
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.static_website_bucket.id}/*"
-    }
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
   ]
-}
-EOF
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
