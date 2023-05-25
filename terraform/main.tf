260412972394e0679dddea6336091b98c24aef19
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 02:14:49 -0700

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,23 @@ terraform {
 
 resource "aws_s3_bucket" "static_website_bucket" {
   bucket = "dp-hackathon-2023"
+
   website {
     index_document = "index.html"
     error_document = "error.html"
   }
 }
 
+resource "aws_s3_bucket_website_configuration" "static_website_bucket_website_configuration" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  index_document = "index.html"
+  error_document = "error.html"
+}
+
 resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
   bucket = aws_s3_bucket.static_website_bucket.id
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -33,12 +42,4 @@ resource "aws_s3_bucket_policy" "static_website_bucket_policy" {
   ]
 }
 EOF
-}
-
-resource "aws_s3_bucket_public_access_block" "static_website_bucket_public_access_block" {
-  bucket                  = aws_s3_bucket.static_website_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
 }
