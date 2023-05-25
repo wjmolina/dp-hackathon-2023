79ebe9d2d6a673cf0c0f0fb16b45017c9e9de9bd
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 01:43:37 -0700

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,4 +11,8 @@ terraform {
 
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "dp-hackathon-2023"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
