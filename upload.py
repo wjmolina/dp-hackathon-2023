97655cb52a72d739e34e73aed5a66e7418e436d7
wjmolina <molina.wilfredo.j@gmail.com>
2023-05-25 07:14:28 -0700

    test

--- upload.py
+++ upload.py
@@ -28,8 +28,6 @@ for root, _, files in os.walk(local_directory):
         if file_name.endswith(".tar.gz"):
             local_file = os.path.join(root, file_name)
             key = os.path.relpath(local_file, local_directory).replace(os.path.sep, "/")
-            key = key.split("/")
-            key = "/".join(key[:-1]).replace("-", "_") + "/" + key[-1]
 
             with open(local_file, "rb") as body:
                 s3.put_object(
